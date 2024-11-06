31c7c6b14fde77524b95ca76bcdae9555710919d
João Pedro Pizoli Carvalho <joao.carvalho@mococa.com>
2024-11-06 13:47:53 -0300

    trocando o host do connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const knex = require('knex')({
     client: 'mysql2', 
     connection: {
-        host: process.env.DB_HOST,
+        host: 'dbfacul.c1k4e8ay2llk.us-east-2.rds.amazonaws.com',
         port: 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
