fc03386de0a2eec0d812a75c411e8714157603aa
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-11-10 22:56:08 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.use(express.json()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); 
 app.use(router); 
 
 app.use(cors({
