00c86ae007c5a4b2ff68e5e71f4a524b5ea9624d
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-11-10 23:55:35 -0300

    vs

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
-require('dotenv').config()
+require('dotenv').config();
+
 const express = require('express');
 const router = require('./routers/routers');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false })); 
-app.use(router); 
 
 app.use(cors({
-    origin:'https://front-end-projeto-bf.vercel.app/',
-  }));
+    origin: 'https://front-end-projeto-bf.vercel.app',
+}));
 
-app.listen(8080, () => {
+app.use(express.json());
+app.use(router);
+
+app.listen(8080, '0.0.0.0', () => {
     console.log('API rodando!');
 });
