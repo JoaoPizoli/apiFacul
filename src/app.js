5dd64ddd385210e617294a5e42cc470dea37d3f7
João Pedro Pizoli Carvalho <joao.carvalho@mococa.com>
2024-11-11 13:16:10 -0300

    porta 8080

--- src/app.js
+++ src/app.js
@@ -13,6 +13,6 @@ app.use(cors({
 app.use(express.json());
 app.use(router);
 
-app.listen(22, '0.0.0.0', () => {
-  console.log('API rodando na porta 22');
+app.listen(8080, '0.0.0.0', () => {
+  console.log('API rodando na porta 8080');
 });
