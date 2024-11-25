a3f49e276a2fac8da20ec70c367e5ae85c03f3de
JoaoPedroPizoli <pizoliejesus@gmail.com>
2024-11-25 16:51:48 -0300

    funfa pls

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -11,13 +11,22 @@ class UsuarioController {
                 return res.status(404).json({ message: 'Usuário não encontrado.' });
             }
 
-            return res.status(200).json({
+            let responseData = {
                 id: user.id,
                 nome: user.nome,
                 email: user.email,
                 phoneNumber: user.phoneNumber,
                 role: user.role
-            });
+            };
+
+            if (user.role === 'professor') {
+                const professor = await knex('professores').where({ email: user.email }).first();
+                if (professor) {
+                    responseData.areaAtuacao = professor.areaAtuacao;
+                }
+            }
+
+            return res.status(200).json(responseData);
         } catch (error) {
             console.error('Erro ao obter informações do usuário:', error);
             return res.status(500).json({ message: 'Erro ao obter informações do usuário.' });
@@ -26,16 +35,27 @@ class UsuarioController {
 
     // Método para atualizar informações do usuário atual
     async updateMe(req, res) {
-        const { nome, email, phoneNumber, password } = req.body;
+        const { nome, email, phoneNumber, password, senhaNova } = req.body;
         try {
+            // Obter o usuário atual
+            const user = await knex('usuarios').where({ id: req.user.id }).first();
+            if (!user) {
+                return res.status(404).json({ message: 'Usuário não encontrado.' });
+            }
+
+            // Verificar a senha atual
+            if (!password || !bcrypt.compareSync(password, user.password)) {
+                return res.status(400).json({ message: 'Senha atual incorreta.' });
+            }
+
             // Preparar os dados para atualização
             const updateData = {};
             if (nome) updateData.nome = nome;
             if (email) updateData.email = email;
             if (phoneNumber) updateData.phoneNumber = phoneNumber;
-            if (password) {
+            if (senhaNova) {
                 const salt = bcrypt.genSaltSync(10);
-                const hashedPassword = bcrypt.hashSync(password, salt);
+                const hashedPassword = bcrypt.hashSync(senhaNova, salt);
                 updateData.password = hashedPassword;
             }
 
